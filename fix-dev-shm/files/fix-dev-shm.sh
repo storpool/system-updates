dca51b4f6c31a80da2bf0cdcb57ec2d611ef782d
Andriy Skovorodnikov <andriy.skov@storpool.com>
2021-12-20 15:36:36 +0000

    fix-dev-shm script simplified

--- fix-dev-shm/files/fix-dev-shm.sh
+++ fix-dev-shm/files/fix-dev-shm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -o errexit
 # unmount of redundant mounts
-number_of_mounts=$(findmnt -lnk /dev/shm | wc -l)
-while [ ${number_of_mounts} -gt 1 ]
+while [ $(findmnt -lnk /dev/shm | wc -l) -gt 1 ]
 do
     umount /dev/shm
-    number_of_mounts=$(findmnt -lnk /dev/shm | wc -l)
 done
